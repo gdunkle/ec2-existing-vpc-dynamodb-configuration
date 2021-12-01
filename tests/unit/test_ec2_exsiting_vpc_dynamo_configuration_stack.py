3d59b5fcdbd7964ecf8024fe961e3909e1148968
Galen Dunkleberger <awsgalen@amazon.com>
2021-12-01 12:07:27 -0500

    Initial commit

---
+++ tests/unit/test_ec2_exsiting_vpc_dynamo_configuration_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from ec2_exsiting_vpc_dynamo_configuration.ec2_exsiting_vpc_dynamo_configuration_stack import Ec2ExsitingVpcDynamoConfigurationStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ec2_exsiting_vpc_dynamo_configuration/ec2_exsiting_vpc_dynamo_configuration_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = Ec2ExsitingVpcDynamoConfigurationStack(app, "ec2-exsiting-vpc-dynamo-configuration")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
